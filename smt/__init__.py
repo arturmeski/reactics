bd06d575f743a2127eec69c7d3794c8bf29fde2f
Artur Meski <artur@Arturs-MacBook-Pro.local>
2016-12-28 22:15:40 +0100

    SmtChecker class moved to legacy

--- smt/__init__.py
+++ smt/__init__.py
@@ -1,4 +1,4 @@
-from smt.smt_checker import SmtChecker
+#from smt.smt_checker import SmtChecker
 from smt.smt_checker_rs import SmtCheckerRS
 from smt.smt_checker_rsc import SmtCheckerRSC
 from smt.smt_checker_rs_na import SmtCheckerRSNA
