1213321c3be18968b7f99b8c39eb6b2a3c3578e8
Artur Meski <artur.meski@gmail.com>
2018-02-10 12:48:42 +0000

    Migrated to new CUDD (3.0.0) -- complete

--- build_cudd.sh
+++ build_cudd.sh
@@ -3,7 +3,11 @@
 CUDD_VERSION=3.0.0
 INSTALL_DIR="$PWD"
 
+tar -zxvf dist/cudd-$CUDD_VERSION.tar.gz
 cd cudd-$CUDD_VERSION
 ./configure --enable-shared --enable-obj --prefix=$INSTALL_DIR/cudd
-make
+make -j4
 make install
+cd ..
+rm -rf cudd-$CUDD_VERSION
+
