28821c78de793fd30cbba08394d8fb28019d1fb5
Artur Meski <artur.meski@gmail.com>
2019-02-22 23:23:59 +0000

    Cleaning up

--- results/gen_dat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for logfile in *.log; do
-
-    log_base_name=$(basename -s .log $logfile)
-    dat_output="${log_base_name}.dat"
-    cut -d, -f 1,3 $logfile | tr -d '()' | tr ',' '\t' > $dat_output
-
-done
