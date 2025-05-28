90c8e4f4c071cab408cd1118f830596badf713ef
Marcin Piątkowski <marcin.piatkowski@mat.umk.pl>
2025-05-28 16:21:56 +0200

    Added time and memory measurement for windows. (#4)

--- reactics-bdd/memtime.hh
+++ reactics-bdd/memtime.hh
@@ -5,7 +5,12 @@
 #define __MEMTIME_H__
 
 #include <sys/time.h>
+#ifdef __WIN32__
+#include <windows.h>
+#include <psapi.h>
+#else
 #include <sys/resource.h>
+#endif
 #if defined(__APPLE__)
 #include <malloc/malloc.h>
 #endif
@@ -17,12 +22,33 @@ using namespace std;
 
 typedef long long int64;
 
+#ifdef __WIN32__
+
+static inline double cpuTime() 
+{
+    FILETIME createTime, exitTime, kernelTime, userTime;
+
+    if (!GetProcessTimes(GetCurrentProcess(), &createTime, &exitTime, &kernelTime, &userTime)) {
+        return 0.0;
+    }
+
+    ULARGE_INTEGER u;
+    u.LowPart = userTime.dwLowDateTime;
+    u.HighPart = userTime.dwHighDateTime;
+
+    return (double)u.QuadPart / 10e6;
+}
+
+#else
+
 static inline double cpuTime(void)
 {
   struct rusage ru;
   getrusage(RUSAGE_SELF, &ru);
   return (double)ru.ru_utime.tv_sec + (double)ru.ru_utime.tv_usec / 1000000;
 }
+
+#endif
 
 static inline int memReadStat(void)
 {
@@ -47,10 +73,27 @@ static inline int memReadStat(void)
   return value;
 }
 
+
+#ifdef __WIN32__
+
+static inline int64 memUsedInt64() 
+{
+    PROCESS_MEMORY_COUNTERS pmc;
+    if (GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc))) {
+        return (int64) pmc.WorkingSetSize;
+    }
+    
+    return 0;
+}
+
+#else
+
 static inline int64 memUsedInt64()
 {
   return (int64)memReadStat() * (int64)getpagesize();
 }
+
+#endif
 
 #if defined(__APPLE__)
 
